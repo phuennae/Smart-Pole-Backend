8a68c049563bbfba9aab40a2ce73a14bbc741d58
phuennae <liverpoolnae@gmail.com>
2026-07-21 15:16:41 +0700

    edited get_node.php

--- get_nodes.php
+++ get_nodes.php
@@ -9,13 +9,14 @@ $formatted_nodes = array();
 foreach ($nodes as $row) {
     // ใช้ ?? 0 เพื่อกำหนดค่าเริ่มต้นเป็น 0 ถ้าใน DB เป็น NULL
     $formatted_nodes[] = [
-        "id"         => $row['id'],
-        "name"       => $row['name'] ?? 'No Name',
-        "ip_address" => $row['ip_address'] ?? '',
-        "port"       => $row['port'] ?? '80',
-        "lat"        => (float)($row['latitude'] ?? 0),
-        "lng"        => (float)($row['longitude'] ?? 0),
-        "status"     => "online"
+        "id"          => $row['id'],
+        "name"        => $row['name'] ?? 'No Name',
+        "ip_address"  => $row['ip_address'] ?? '',
+        "port"        => $row['port'] ?? '80',
+        "lat"         => (float)($row['latitude'] ?? 0),
+        "lng"         => (float)($row['longitude'] ?? 0),
+        "last_volume" => $row['last_volume'] ?? 80, 
+        "status"      => "online"
     ];
 }
 
